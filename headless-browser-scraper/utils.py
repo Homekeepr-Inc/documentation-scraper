7d7b8a0e3fdb3dd8194674e81cbf7d8833455c9d
dawson-homekeepr <dawson@homekeepr.co>
2025-09-25 14:09:38 -0700

    reduce scraper window size

--- headless-browser-scraper/utils.py
+++ headless-browser-scraper/utils.py
@@ -40,7 +40,7 @@ def get_chrome_options(download_dir=None):
     options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--window-size=1280,720')
     options.add_argument('--disable-popup-blocking')
     options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
 
