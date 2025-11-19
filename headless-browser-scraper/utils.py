b71ad230923c5b7eea7d6460c0f01560e80c1ad2
dawson-homekeepr <dawson@homekeepr.co>
2025-11-19 14:31:03 -0800

    stop using headlesschrome

--- headless-browser-scraper/utils.py
+++ headless-browser-scraper/utils.py
@@ -126,7 +126,8 @@ def get_chrome_options(download_dir=None):
     options.add_argument('--disable-images')
     options.add_argument('--disable-plugins')
     options.add_argument('--disable-extensions')
-    # options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
+    # NEVER use the HeadlessChrome user-agent. Cloudflare will force a captcha on us.
+    options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.3')
 
     if PROXY_URL:
         options.add_argument(f'--proxy-server={PROXY_URL}')
